e8de3bda0e661b69933ed469c8fccdd5537d245c
SpeedHunterSam <brian.s2@gmail.com>
2019-08-06 08:13:59 -0500

    game working cleaning up code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,29 +1,33 @@
 //initialize variables and values needed
 
-
-const goalValue = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
-const crystal1Value = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
-const crystal2Value = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
-const crystal3Value = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
-const crystal4Value = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
+let goalValue = Math.floor(Math.random() * (120 - 19 + 1) ) + 19;
+let crystal1Value = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
+let crystal2Value = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
+let crystal3Value = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
+let crystal4Value = Math.floor(Math.random() * (12 - 1 + 1) ) + 1;
 
 
 let liveScore = 0;
 let wins = 0;
 let losses = 0;
 
-
+//this function runs once the user is ready to start the game
 
 function startGame(){
 
 
-    alert("game started");
+    console.log("game started");
     console.log(goalValue);
     console.log(crystal1Value);
     console.log(crystal2Value);
     console.log(crystal3Value);
     console.log(crystal4Value);
 
+        //display inital goal value to viewport
+
+    document.getElementById("yourNumber").innerHTML = "<strong>"+ goalValue + "</strong>";
+
+    //listen for click on each of the four crystals then update the accumulated value with the value of the most recent click then update scoreboard
 
     document.getElementById("crystal1").addEventListener("click", function () {
         //update livescore when crystal 1 is clicked
@@ -59,6 +63,7 @@ function startGame(){
 
 }
 
+//this function runs after each user click
 
 function updateScoreboard() {
     console.log("Goal Value: " + goalValue);
@@ -67,12 +72,40 @@ function updateScoreboard() {
     if (liveScore === goalValue) {
         wins++;
         console.log("Wins: " + wins);
+        //reset random numbers to play again
+        goalValue = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+        crystal1Value = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        crystal2Value = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        crystal3Value = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        crystal4Value = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        liveScore = 0;
+
+        //display new goal value to viewport
+        document.getElementById("yourNumber").innerHTML = goalValue;
+
+        //display updated wins & losses to viewport
+        document.getElementById("scoreBoard").innerHTML = "<strong>Wins: "+ wins + "<br/>Losses: " + losses + "</strong>";
 
     }
     if (liveScore > goalValue) {
         losses++;
         console.log("Losses: " + losses);
+        //reset random numbers to play again
+        goalValue = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+        crystal1Value = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        crystal2Value = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        crystal3Value = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        crystal4Value = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+        liveScore = 0;
+        //display new goal value to viewport
+
+        document.getElementById("yourNumber").innerHTML = goalValue;
+
+        //display updated wins & losses to viewport
+        document.getElementById("scoreBoard").innerHTML = "<strong>Wins: " + wins + "<br/>Losses: " + losses + "</strong>";
     }
+
+    document.getElementById("liveScore").innerHTML = liveScore;
 }
 
 
