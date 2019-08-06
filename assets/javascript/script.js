f893ec16b05228ec56d7e21a2d4bbb708fc8c54e
SpeedHunterSam <brian.s2@gmail.com>
2019-08-06 07:42:01 -0500

    basic scoring logic complete

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -24,47 +24,56 @@ function startGame(){
     console.log(crystal3Value);
     console.log(crystal4Value);
 
-//execute loop while liveScore is less than goalValue
-   while(liveScore < goalValue){
 
-        document.getElementById("crystal1").addEventListener("click", function(){
-                //update livescore when crystal 1 is clicked
-            liveScore = liveScore + crystal1Value;
-        });
-        document.getElementById("crystal2").addEventListener("click", function(){
-                //update livescore when crystal 2 is clicked
-            liveScore = liveScore + crystal2Value;
-        });
-        document.getElementById("crystal3").addEventListener("click", function(){
-            //update livescore when crystal 3 is clicked
+    document.getElementById("crystal1").addEventListener("click", function () {
+        //update livescore when crystal 1 is clicked
+        liveScore = liveScore + crystal1Value;
+        console.log("Crystal1 Clicked, Value: " + crystal1Value);
+        
+        updateScoreboard();
+    });
+
+    document.getElementById("crystal2").addEventListener("click", function () {
+        //update livescore when crystal 2 is clicked
+        liveScore = liveScore + crystal2Value;
+        console.log("Crystal2 Clicked, Value: " + crystal2Value);
+        updateScoreboard();
+
+       
+    });
+
+    document.getElementById("crystal3").addEventListener("click", function () {
+        //update livescore when crystal 3 is clicked
         liveScore = liveScore + crystal3Value;
-        });
-        document.getElementById("crystal4").addEventListener("click", function(){
-            //update livescore when crystal 4 is clicked
+        console.log("Crystal3 Clicked, Value: " + crystal3Value);
+        updateScoreboard();
+
+    });
+
+    document.getElementById("crystal4").addEventListener("click", function () {
+        //update livescore when crystal 4 is clicked
         liveScore = liveScore + crystal4Value;
-        });
-
-        console.log(goalValue);
-        console.log(liveScore);
-
-    }
-
-    if(liveScore === goalValue){
-         wins++;
-        console.log("Wins: " + wins);
-
-    }else if(liveScore > goalValue){
-        losses++;
-        console.log("Losses: " + losses);
-
-
-    }
-
+        console.log("Crystal4 Clicked, Value: " + crystal4Value);
+        updateScoreboard();
+    });
 
 }
 
 
+function updateScoreboard() {
+    console.log("Goal Value: " + goalValue);
+    console.log("Live Score: " + liveScore);
 
+    if (liveScore === goalValue) {
+        wins++;
+        console.log("Wins: " + wins);
+
+    }
+    if (liveScore > goalValue) {
+        losses++;
+        console.log("Losses: " + losses);
+    }
+}
 
 
 document.getElementById("allCrystals").addEventListener("click", startGame); //listen for initialization click to start the game
